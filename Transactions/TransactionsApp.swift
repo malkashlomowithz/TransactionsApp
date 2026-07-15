2fdb44f0fd3c3b64a1fc60fbf6db94346f57634c
malka0987654@gmail.com <malka0987654@gmail.com>
2026-07-15 10:31:40 +0300

    Initial Commit

---
+++ Transactions/TransactionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionsApp.swift
+//  Transactions
+//
+//  Created by Malky Shlomowitz on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TransactionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
